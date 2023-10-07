1061b33cb8da0647e2dc81bc551ce24ce858a0c0
jhon03 <hoyosb90@gmail.com>
2023-10-06 21:35:22 -0500

    creación de componente para apadrinamiento

---
+++ src/app/donaciones/apadrinamiento/apadrinamiento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apadrinamiento',
+  templateUrl: './apadrinamiento.component.html',
+  styleUrls: ['./apadrinamiento.component.css']
+})
+export class ApadrinamientoComponent {
+
+}
