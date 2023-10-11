9d961096f803f90c95e2d96906b72d0f8e2b2b5c
jhon03 <hoyosb90@gmail.com>
2023-10-10 21:55:03 -0500

    modulo de autenticacion

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+
+const routes: Routes = [
+
+
+  {
+    path: '',
+    component: RegistroComponent
+  },
+  
+  {
+    path: 'Registro',
+    component: RegistroComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
