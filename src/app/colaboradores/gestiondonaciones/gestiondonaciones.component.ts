570d76f624ba65deacdae8c86fc630ed9b86fabd
jhon03 <hoyosb90@gmail.com>
2023-10-16 12:05:21 -0500

    panel para gestión de las donaciones

--- src/app/colaboradores/gestiondonaciones/gestiondonaciones.component.ts
+++ src/app/colaboradores/gestiondonaciones/gestiondonaciones.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   MatBottomSheet,
   MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
-
-
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AutenticacionService } from 'src/app/autenticacion.service';
+import { LoginService } from 'src/app/login.service';
 @Component({
   selector: 'app-gestiondonaciones',
   templateUrl: './gestiondonaciones.component.html',
   styleUrls: ['./gestiondonaciones.component.css']
 })
-export class GestiondonacionesComponent {
+export class GestiondonacionesComponent implements OnInit, OnDestroy{
+
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  
+  constructor(private authService: AutenticacionService, private router:Router, private loginService: LoginService){}
+
+
+  
 
   
 }
- 
